5870f7b13977049ffb10bd43360021ee68edba2a
Justin <justinkaashoek@college.harvard.edu>
2017-12-02 11:13:58 -0500

    Checkpoint

---
+++ application.py
@@ -0,0 +1,153 @@
+from flask import Flask, flash, redirect, render_template, request, session, g
+from flask_socketio import SocketIO
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions
+from werkzeug.security import check_password_hash, generate_password_hash
+import sqlite3
+
+from helpers import apology, login_required, get_db, make_dicts, query_db
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+socketio = SocketIO(app)
+Session(app)
+
+if __name__ == '__main__':
+    socketio.run(app)
+
+@app.route("/")
+@login_required
+def index():
+    return render_template("index.html")
+
+@socketio.on('my event')
+def handle_my_custom_event(json):
+    print('received json: ' + str(json))
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Query database for username
+        rows = query_db("SELECT * FROM users WHERE username = ?",
+                          [request.form.get("username")], one=True)
+
+        # Ensure username exists and password is correct
+        if rows == None or not check_password_hash(rows["hash"], request.form.get("password")):
+            return apology("invalid username and/or password", 403)
+   
+        # Remember which user has logged in
+        session["user_id"] = rows["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+@app.route("/about", methods=["GET"])
+def about():
+    """Redirect to about page"""
+
+    # Redirect user to about page
+    return render_template("about.html")
+
+@app.route("/chat", methods=["GET"])
+@login_required
+def chat():
+    """Redirect to chat page"""
+    users = query_db("SELECT * FROM users")
+    return render_template("chat.html", users=users)
+
+@app.route("/discussions", methods=["GET"])
+@login_required
+def discussions():
+    """Redirect to about page"""
+
+    # Redirect user to about page
+    return render_template("discussions.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username")
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password")
+
+        # Ensure passwords match
+        elif not request.form.get("password") == request.form.get("confirmation"):
+            return apology("passwords do not match")
+
+        # Generate password hash
+        hash = generate_password_hash(request.form.get("password"))
+
+        # Add user into users table
+        result = db.execute("INSERT INTO users (username, hash) VALUES (:username, :hash)",
+                            username=request.form.get("username"), hash=hash)
+
+        # Handle case if username already exists
+        if not result:
+            return apology("username already exists")
+
+        # Set the user's id
+        session["user_id"] = result
+
+        # Redirect user to home page
+        return redirect("/")
+
+    else:
+        return render_template("register.html")
+
+def errorhandler(e):
+    """Handle error"""
+    return apology(e.name, e.code)
+
+
+# listen for errors
+for code in default_exceptions:
+    app.errorhandler(code)(errorhandler)
---
+++ helpers.py
@@ -0,0 +1,52 @@
+import csv
+import sqlite
+
+from flask import redirect, render_template, request, session, g
+from functools import wraps
+
+DATABASE = 'radar.db'
+
+def apology(message, code=400):
+    """Renders message as an apology to user."""
+    def escape(s):
+        """
+        Escape special characters.
+
+        https://github.com/jacebrowning/memegen#special-characters
+        """
+        for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
+                         ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
+            s = s.replace(old, new)
+        return s
+    return render_template("apology.html", top=code, bottom=escape(message)), code
+
+
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    http://flask.pocoo.org/docs/0.12/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    db.row_factory = sqlite3.Row
+    return db
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+def make_dicts(cursor, row):
+    return dict((cursor.description[idx][0], value)
+                for idx, value in enumerate(row))
